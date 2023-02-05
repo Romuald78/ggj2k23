a7e594a7435cb5b9ae70cd1d813156123e8ff061
Romuald GRIGNON <romuald.grignon@gmail.com>
2023-02-05 03:56:52 +0100

    add HP bar

--- classes/HPBar.py
+++ classes/HPBar.py
@@ -22,17 +22,17 @@ class HPBar:
         self.sprite.center_x = self.camera.x - (self.camera.W // 2) + (self.sprite.width // 2)
         self.sprite.center_y = self.camera.y - (self.camera.H // 2) + (self.sprite.height // 2)
 
-        percent = (self.player.hp / self.player.maxHP) * 170
+        percent = (self.player.hp / self.player.maxHP)
 
-        arcade.draw_rectangle_filled(self.sprite.center_x+10, self.sprite.center_y - 15, 150, 40,arcade.color.REGALIA,tilt_angle=80)
-        arcade.draw_rectangle_filled(self.sprite.center_x+10, self.sprite.center_y - 15, percent, 40,arcade.color.BLUE_GRAY,tilt_angle=80)
+        arcade.draw_rectangle_filled(self.sprite.center_x+10, self.sprite.center_y - 15, 150, 40,(96,96,96,256),tilt_angle=80)
+
+        ymid = self.sprite.center_y - (1-percent) * 142 / 2
+        arcade.draw_rectangle_filled(self.sprite.center_x+12, ymid-13, 40, 142*percent,arcade.color.BLUE_GRAY,tilt_angle=-10)
 
         self.sprite.draw()
 
-
     def update(self, deltatime):
-
 
         if(self.player.hp < 0):
             print("You Died")
-            self.player.hp += 100000000
+            self.player.hp = 10000000
